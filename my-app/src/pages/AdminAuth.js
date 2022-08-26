7d177058c0c2669e2c79d8f2fe78ec7c9d0d1838
RaghuRaikar <96157136+RaghuRaikar@users.noreply.github.com>
2022-08-26 15:10:57 -0700

    Removed Admin Login

--- my-app/src/pages/AdminAuth.js
+++ my-app/src/pages/AdminAuth.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import { auth, provider } from "../firebase-config";
-import { signInWithPopup } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
 
-function AdminAuth({ setIsUser }) {
-    let navigate = useNavigate();
-  
-    const signInWithGoogle = () => {
-      signInWithPopup(auth, provider).then((result) => {
-        localStorage.setItem("isUser", true);
-        setIsUser(true);
-        navigate("/");
-      });
-    };
-  
-    return (
-      <div className="loginPage">
-        <p> Admin Login/Signup </p>
-        <button className="login-with-google-btn" onClick={signInWithGoogle}>
-          Sign in with Google
-        </button>
-      </div>
-    );
-  }
-  
-  export default AdminAuth;
+ const AdminAuth = () => {
+     return (
+         <div>
+             Work on Admin Auth Feature Here.
+         </div>
+     );
+ };
+
+ export default AdminAuth; 
