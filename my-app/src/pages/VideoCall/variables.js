d6fd7fbd20f3eb69e07fa6a69f845fc65f7b4665
RaghuRaikar <96157136+RaghuRaikar@users.noreply.github.com>
2022-08-31 19:03:24 -0700

    Finished VideoCall

---
+++ my-app/src/pages/VideoCall/variables.js
@@ -0,0 +1,3 @@
+export const ID="a46012f3eabf498c94f70c574ed61926"
+export const TOKEN="007eJxTYNB8UHz5+ra1ZbzJl4tduRYemF3/hKH5dw6DVY2B823d57cVGBJNzAwMjdKMUxOT0kwsLZItTdLMDZJNzU1SU8wMLY3MHi77l5T443/S7Jc3WRkZIBDEZ2HITczMY2AAAPk1JYY="
+export const URL = "https://whereby.com/mindpalaceproject?utm_source=onboarding&utm_content=link"
