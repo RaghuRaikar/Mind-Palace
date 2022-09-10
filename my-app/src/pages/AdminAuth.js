997a3a05599bb6e87bea560c1aaea0529241ebcf
Solomon <bangiyevs@gmail.com>
2022-09-10 15:00:35 -0400

    updated admin login page

--- my-app/src/pages/AdminAuth.js
+++ my-app/src/pages/AdminAuth.js
@@ -2,6 +2,8 @@ import React from "react";
 import { auth, provider } from "../firebase-config";
 import { signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import "../styleSheets/AdminLogin.css";
+import loginLogo from "../icons/admin-login-logo.png";
 
 function AdminAuth({ setIsUser }) {
   let navigate = useNavigate();
@@ -15,12 +17,28 @@ function AdminAuth({ setIsUser }) {
   };
 
   return (
-    <div className="loginPage">
-      <p> Admin Login/Signup </p>
-      <button className="login-with-google-btn" onClick={signInWithGoogle}>
-        Sign in with Google
-      </button>
+    <div className="admin-login-screen">
+      <div className="admin-login-screen-left">
+        <img src={loginLogo} alt="Logo"></img>
+        <h1>The Shoe Hut</h1>
+      </div>
+      <div className="admin-login-screen-right">
+        <form className="admin-login-form">
+          <h1>Welcome back</h1>
+          <label>Email</label>
+          <input placeholder="name@email.com" type="email"></input>
+          <label>Password</label>
+          <input type="password"></input>
+          <button className="admin-login-button">Sign in</button>
+          <button className="login-with-google-btn" onClick={signInWithGoogle}>
+            Sign in with Google
+          </button>
+        </form>
+      </div>
     </div>
+    // <div className="loginPage">
+
+    // </div>
   );
 }
 
